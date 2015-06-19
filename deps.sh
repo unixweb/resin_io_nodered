a661ada314af8e850c0cf659e1e71e3bbd916e8e
vergissberlin <vergissberlin@googlemail.com>
2015-06-19 21:57:50 +0200

    Test copy files into the container

--- deps.sh
+++ deps.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 apt-get -q update
-dpkg -i python-rpi.gpio_0.5.11-1_armhf.deb
+#wget http://archive.raspberrypi.org/debian/pool/main/r/rpi.gpio/python-rpi.gpio_0.5.11-1_armhf.deb
+#dpkg -i python-rpi.gpio_0.5.11-1_armhf.deb
+
+wget http://archive.raspberrypi.org/debian/pool/main/r/rpi.gpio/python-rpi.gpio_0.5.8-1_armhf.deb
+dpkg -i python-rpi.gpio_0.5.8-1_armhf.deb
+which python
 
 npm install -g --unsafe-perm \
 						pm2 \
 						node-red
-
-cp -f ../settings.js .node-red
-cp -f ../flows_raspberrypi.json .node-red
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-node-red-pi --max-old-space-size=128 --userDir /app/my-node-red -v
+node-red-pi --max-old-space-size=128 --userDir /app/node-red -v
 #pm2 start /usr/local/bin/node-red-pi -- -v
