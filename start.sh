693b328c4fd08e6fc7ba58aae8b0e911ddf3e5cb
vergissberlin <vergissberlin@googlemail.com>
2015-06-20 10:05:37 +0200

    Test copy files into the container

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-node-red-pi --max-old-space-size=128 --userDir /app/node-red -v
+node-red-pi --max-old-space-size=128 -v
+#node-red-pi --max-old-space-size=128 --userDir /app/node-red -v
 #pm2 start /usr/local/bin/node-red-pi -- -v
