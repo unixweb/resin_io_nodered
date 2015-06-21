dfc55258589e5dd45dd1dddbc6343e86626696c7
vergissberlin <vergissberlin@googlemail.com>
2015-06-21 16:30:58 +0200

    Using own flows

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-node-red-pi --max-old-space-size=128 -v
+node-red-pi -v
 #node-red-pi --max-old-space-size=128 --userDir /usr/src/app/node-red/ -v
 #pm2 start /usr/local/bin/node-red-pi -- -v
 
