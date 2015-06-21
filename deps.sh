9f1f6e2d90f2dd850308cfe42604e7cd1668a174
vergissberlin <vergissberlin@googlemail.com>
2015-06-21 11:37:43 +0200

    Using own flows

--- deps.sh
+++ deps.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 apt-get -q update
-#wget http://archive.raspberrypi.org/debian/pool/main/r/rpi.gpio/python-rpi.gpio_0.5.11-1_armhf.deb
-#dpkg -i python-rpi.gpio_0.5.11-1_armhf.deb
 
 wget http://archive.raspberrypi.org/debian/pool/main/r/rpi.gpio/python-rpi.gpio_0.5.8-1_armhf.deb
 dpkg -i python-rpi.gpio_0.5.8-1_armhf.deb
-which python
+
+#which python
 
 npm install -g --unsafe-perm \
 						pm2 \
