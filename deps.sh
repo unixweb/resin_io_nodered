73b539a4c6bb0ae4e6a79758c1ac43f3a7a9f799
vergissberlin <vergissberlin@googlemail.com>
2015-06-21 17:15:54 +0200

    Using own flows

--- deps.sh
+++ deps.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 apt-get -q update
-apt-get -q -y upgrade
-apt-get -q -y install python python-dev
 wget http://archive.raspberrypi.org/debian/pool/main/r/rpi.gpio/python-rpi.gpio_0.5.8-1_armhf.deb
 dpkg -i python-rpi.gpio_0.5.8-1_armhf.deb
 python --version
 
 npm install -g --unsafe-perm \
-			pm2 \
-			node-red
+#			pm2 \
+			node-red@0.10.8
 
 #cp /usr/src/app/flows-raspberrypi.json /root/.node-red/flows-$HOSTNAME.json
 #cp /usr/src/app/flows-raspberrypi.json /root/.node-red/flows-test.json
