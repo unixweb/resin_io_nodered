fd24b86b826c2e19405bacef2ef802145a1a0ef7
Joachim Hummel <joachim.hummel@gmail.com>
2017-02-14 19:56:40 +0100

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -4,7 +4,7 @@ apt-get -q update
 
 wget http://archive.raspberrypi.org/debian/pool/main/r/rpi.gpio/python-rpi.gpio_0.5.8-1_armhf.deb
 dpkg -i python-rpi.gpio_0.5.8-1_armhf.deb
-mkdir /home/cam/webcams
+mkdir -p /home/cam/webcams
 
 npm install -g --unsafe-perm \
 			node-red
